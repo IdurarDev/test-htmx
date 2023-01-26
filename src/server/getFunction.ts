b33fe1741427e8fbdcee46b41c00ad46806621ff
Hamid Berkaine <hamiddev78@gmail.com>
2023-01-26 11:22:38 +0100

    update file for the display which the view page according to the corresponding page

--- src/server/getFunction.ts
+++ src/server/getFunction.ts
@@ -1,7 +1,21 @@
 import type http from 'http'
-// import { routing } from '../routes/Routes'
+import { routing } from '../routes/Route'
 // import { handleFileReauest } from './fileRequest'
 
 export function get(req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) {
     console.info(`New request from ${req.url}`)
+
+    const content = routing(req)
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(`<!doctype html>
+        <html lang="fr">
+            <head>
+                <meta charset="utf-8" />
+                <title>Test HTMX</title>
+            </head>
+            <body>
+                ${content}
+            </body>
+
+    `)
 }
